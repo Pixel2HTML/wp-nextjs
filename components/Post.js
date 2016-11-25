c25b65c3ac1de74d7694f11f75a1ab5685575787
Diego Peralta <diego.peralta.dev@gmail.com>
2016-11-25 17:29:07 -0300

    add Post component

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default ({ id, title, link, excerpt }) => (
+  <div>
+    <h1>
+      <Link href={ link }>{ title }</Link>
+    </h1>
+    <div dangerouslySetInnerHTML={{__html: excerpt}}>
+
+    </div>
+  </div>
+)
