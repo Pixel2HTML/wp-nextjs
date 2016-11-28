f2aa7d31e4efaba1358767d52017e09c4136bbf6
Miguel Palau <mpalauzarza@gmail.com>
2016-11-28 12:54:43 -0600

    added global css styles

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import fetch from 'isomorphic-fetch'
-import Link from 'next/link'
+import Header from '../components/Header'
 import Post from '../components/Post'
 
 export default class extends React.Component {
-  static async getInitialProps() {
-    const res   = await fetch('http://wp.pixel2html.com/examples/nextjs/wp-json/wp/v2/posts')
+  static async getInitialProps () {
+    const res = await fetch('http://wp.pixel2html.com/examples/nextjs/wp-json/wp/v2/posts')
     const posts = await res.json()
     return { posts: posts }
   }
-  render(){
-    return(
+  render () {
+    return (
       <div>
+        <Header />
         {
-          this.props.posts.map( (post, i) => (
+          this.props.posts.map((post, i) => (
             <Post id={post.id} title={post.title.rendered} link={post.link} excerpt={post.excerpt.rendered} />
           ))
         }
