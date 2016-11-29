34dc795857eb93d3fe98b0cb4b97149adcb55024
Miguel Palau <mpalauzarza@gmail.com>
2016-11-28 19:57:26 -0600

    adding hero component

---
+++ components/Hero.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <header>
+    <h1>Hello header</h1>
+  </header>
+)
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import fetch from 'isomorphic-fetch'
 import Header from '../components/Header'
 import Post from '../components/Post'
-
+import Hero from '../components/Hero'
 const baseURL = 'http://wp.pixel2html.com/examples/nextjs/'
 const APIendpoint = 'wp-json/wp/v2/'
 
 export default class extends React.Component {
-  static async getInitialProps() {
-    const res = await fetch(baseURL+APIendpoint+'posts')
+  static async getInitialProps () {
+    const res = await fetch(baseURL + APIendpoint + 'posts')
 
     const posts = await res.json()
     return { posts: posts }
@@ -16,12 +16,16 @@ export default class extends React.Component {
   render () {
     return (
       <div>
-        <Header title="My Awesome Pixel Blog"/>
-        {
-          this.props.posts.map((post) => (
-            <Post key={post.id} id={post.id} title={post.title.rendered} link={post.link} excerpt={post.excerpt.rendered} />
-          ))
-        }
+        <Header title='My Awesome Pixel Blog' />
+        <Hero />
+        {this.props.posts.map((post) => (
+           <Post
+             key={post.id}
+             id={post.id}
+             title={post.title.rendered}
+             link={post.link}
+             excerpt={post.excerpt.rendered} />
+         ))}
       </div>
     )
   }
