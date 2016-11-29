af5e17bf4739c85de3a809baf785930b4de37081
Miguel Palau <mpalauzarza@gmail.com>
2016-11-29 14:50:55 -0600

    adding styles to the hero component

---
+++ pages/post.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import fetch from 'isomorphic-fetch'
+import Link from 'next/link'
+import Post from '../components/Post'
+import Header from '../components/Header'
+import * as  _ from 'lodash'
+
+const baseURL = 'http://wp.pixel2html.com/examples/nextjs/'
+const APIendpoint = 'wp-json/wp/v2/'
+
+export default class extends React.Component {
+  static async getInitialProps ({query: { id  }}) {
+    const res = await fetch(baseURL + APIendpoint + 'posts/' + id)
+    const post = await res.json()
+
+    return {
+      post: post
+
+    }
+  }
+  render () {
+    const post = this.props.post
+
+    return (
+      <div>
+        <Header />
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title.rendered}
+          link={post.link}
+          excerpt={post.excerpt.rendered} />
+      </div>
+
+    )
+  }
+
+}
