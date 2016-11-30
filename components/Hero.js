e012ca656ad6724389fc769162731276dbd82fa3
Miguel Palau <mpalauzarza@gmail.com>
2016-11-29 21:10:39 -0600

    made span more readable

--- components/Hero.js
+++ components/Hero.js
@@ -22,7 +22,7 @@ export default class Hero extends React.Component {
             <div className='site-branding'>
               <div className='wrap'>
                 <div className='site-branding-text'>
-                  <h1 className='site-title'><a href='http://wp.pixel2html.com/examples/nextjs/' rel='home'>Project react~nextjs</a></h1>
+                  <h1 className='site-title'><a href='/' rel='home'>Project react~nextjs</a></h1>
                   <p className='site-description'>
                     Just another Pixel2HTML site
                   </p>
@@ -34,7 +34,9 @@ export default class Hero extends React.Component {
                     role='img'
                     viewBox='0 0 43 32'>
                     <path d='M0.356 17.956c0.178 0.178 0.533 0.356 0.711 0.356h33.778l-10.311 10.489c-0.178 0.178-0.356 0.533-0.356 0.711 0 0.356 0.178 0.533 0.356 0.711l1.6 1.6c0.178 0.178 0.533 0.356 0.711 0.356s0.533-0.178 0.711-0.356l14.756-14.933c0.178-0.356 0.356-0.711 0.356-0.889s-0.178-0.533-0.356-0.711l-14.756-14.933c0-0.178-0.356-0.356-0.533-0.356s-0.533 0.178-0.711 0.356l-1.6 1.6c-0.178 0.178-0.356 0.533-0.356 0.711s0.178 0.533 0.356 0.711l10.311 10.489h-33.778c-0.178 0-0.533 0.178-0.711 0.356-0.356 0.178-0.533 0.356-0.533 0.711v2.311c0 0.178 0.178 0.533 0.356 0.711z' />
-                  </svg> <span className='screen-reader-text'>Scroll Down</span></a>
+                  </svg>
+                  <span className='screen-reader-text'>Scroll Down</span>
+                </a>
               </div>
             </div>
           </div>
