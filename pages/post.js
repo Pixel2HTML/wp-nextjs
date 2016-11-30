1b81826ad5bb320dbc4bd0bae543b73e59d65224
Miguel Palau <mpalauzarza@gmail.com>
2016-11-29 20:47:24 -0600

    adding hero everywhere

--- pages/post.js
+++ pages/post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import fetch from 'isomorphic-fetch'
 import Post from '../components/Post'
 import Head from '../components/Head'
+import Hero from '../components/Hero'
 
 const baseURL = 'http://wp.pixel2html.com/examples/nextjs/'
 const APIendpoint = 'wp-json/wp/v2/'
@@ -21,12 +22,15 @@ export default class extends React.Component {
     return (
       <div>
         <Head title={post.title.rendered} />
-        <Post
-          key={post.id}
-          id={post.id}
-          title={post.title.rendered}
-          link={post.link}
-          excerpt={post.excerpt.rendered} />
+        <Hero hasimage />
+        <div className='site-content-contain'>
+          <Post
+            key={post.id}
+            id={post.id}
+            title={post.title.rendered}
+            link={post.link}
+            excerpt={post.excerpt.rendered} />
+        </div>
       </div>
 
     )
