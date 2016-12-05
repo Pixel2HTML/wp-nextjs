7a19a1559982efc1d1aaf518afd6ec4697a591da
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2016-12-05 18:47:40 -0300

    fix

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ export default {
   endpoint: 'http://examples.pixel2html.com/nextjs/wp-json',
   baseURL: 'http://examples.pixel2html.com/nextjs',
   user: 'admin',
-  pw: 'p1x3ldem0'
+  pw: 'p1x3ldemo'
 }
