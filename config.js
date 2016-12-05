dff532994abc8714f758a93d278a50ffffe4f9c7
Miguel Palau <mpalauzarza@gmail.com>
2016-12-05 15:35:04 -0600

    added credentials to wpapi

--- config.js
+++ config.js
@@ -2,5 +2,7 @@
 
 export default {
   endpoint: 'http://examples.pixel2html.com/nextjs/wp-json',
-  baseURL: 'http://examples.pixel2html.com/nextjs'
+  baseURL: 'http://examples.pixel2html.com/nextjs',
+  user: 'admin',
+  pw: 'p1x3ldem0'
 }
