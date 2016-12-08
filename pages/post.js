914538087b30cf622ef44af61565ae142a5f1d02
Miguel Palau <mpalauzarza@gmail.com>
2016-12-08 15:28:24 -0600

    removing fetch from posts component

--- pages/post.js
+++ pages/post.js
@@ -1,21 +1,16 @@
 import React from 'react'
-import fetch from 'isomorphic-fetch'
+import wp, { Site } from '../wp'
+
 import Head from '../components/Head'
 import Hero from '../components/Hero'
-import config from '../config'
 import Main from '../components/Main.js'
 import SinglePost from '../components/SinglePost'
 
 export default class extends React.Component {
   static async getInitialProps ({query: { id }}) {
-    const res = await fetch(config.endpoint + '/wp/v2/posts/' + id)
-    const post = await res.json()
-
-    const res2 = await fetch(config.endpoint)
-    const data = await res2.json()
-
-    const res3 = await fetch(config.endpoint + '/wp/v2/users/' + post.author)
-    const author = await res3.json()
+    const post = await wp.posts().id(id)
+    const data = await Site.root()
+    const author = await wp.users().id(post.author)
 
     return {
       title: data.name,
