70c37ce7ebfb79cce008e8ad81076a38922d934b
Miguel Palau <mpalauzarza@gmail.com>
2016-12-15 14:05:32 -0600

    added a no more posts component

---
+++ components/NoMorePosts.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export default class NoMorePosts extends Component {
+  render () {
+    return (
+      <nav className='navigation pagination' role='navigation'>
+        <h3>No more posts to show...</h3>
+      </nav>
+    )
+  }
+}
