1dcca346d5d27891a3f99f7ca577da8f2b901366
Miguel Palau <mpalauzarza@gmail.com>
2016-12-16 04:22:36 -0600

    state watch on the comments form

--- components/CommentForm.js
+++ components/CommentForm.js
@@ -1,6 +1,18 @@
 import {Component} from 'react'
 
 export default class CommentForm extends Component {
+  state = {
+    comment: '',
+    name: '',
+    email: '',
+    website: ''
+  }
+
+  commentHandler = ev => { this.setState({comment: ev.target.value}) }
+  nameHandler = ev => { this.setState({name: ev.target.value}) }
+  emailHandler = ev => { this.setState({email: ev.target.value}) }
+  websiteHandler = ev => { this.setState({website: ev.target.value}) }
+
   render () {
     return (
       <div id='respond' className='comment-respond'>
@@ -22,6 +34,8 @@ export default class CommentForm extends Component {
               maxLength='65525'
               aria-required='true'
               required='required'
+              value={this.state.comment}
+              onChange={this.commentHandler}
               />
           </p>
           <p className='comment-form-author'>
@@ -35,6 +49,8 @@ export default class CommentForm extends Component {
               maxLength='245'
               aria-required='true'
               required='required'
+              value={this.state.name}
+              onChange={this.nameHandler}
               />
           </p>
           <p className='comment-form-email'>
@@ -49,6 +65,8 @@ export default class CommentForm extends Component {
               aria-describedby='email-notes'
               aria-required='true'
               required='required'
+              value={this.state.email}
+              onChange={this.emailHandler}
               />
           </p>
           <p className='comment-form-url'>
@@ -58,6 +76,8 @@ export default class CommentForm extends Component {
               type='url'
               size='30'
               maxLength='200'
+              value={this.state.website}
+              onChange={this.websiteHandler}
               />
           </p>
           <p className='form-submit'>
