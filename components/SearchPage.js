f0b4968dd014e4139c97b9e2489b995cac9dd905
Miguel Palau <mpalauzarza@gmail.com>
2016-12-25 21:51:11 -0600

    adding the quuery to the search header

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -7,18 +7,19 @@ import { connect } from 'react-redux'
 const mapStoreToProps = store => {
   return {
     title: store.site.root.name,
-    description: store.site.root.description
+    description: store.site.root.description,
+    query: store.search.query
   }
 }
 
 class SearchPage extends Component {
   render () {
-    let {title, description} = this.props
+    let {title, description, query} = this.props
     return (
       <div>
         <Head title={title} />
         <Hero title={title} description={description} hasimage />
-        <Main hasSidebar hasHeader headerTitle='Search Results for: ' >
+        <Main hasSidebar hasHeader headerTitle={`Search Results for: ${query}`} >
           <h2>Search Situation</h2>
         </Main>
       </div>
