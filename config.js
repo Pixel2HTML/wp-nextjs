14cc1577fae2d96fd2e224ee0fe19420467824b4
Miguel Palau <mpalauzarza@gmail.com>
2017-01-04 20:17:23 -0600

    removing useless info from the config
    we don't need to store the user or password as plain text
    nor they are needed on our current setup

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 'use strict'
 
 export default {
-  endpoint: 'https://examples.pixel2html.com/nextjs/wp-json',
-  baseURL: 'https://examples.pixel2html.com/nextjs',
-  user: 'admin',
-  pw: 'p1x3ldemo'
+  endpoint: 'https://examples.pixel2html.com/nextjs/wp-json'
 }
